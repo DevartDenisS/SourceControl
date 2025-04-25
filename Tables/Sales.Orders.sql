535a80a75db3c92013c5dc8249929aea9aa3e6ef
DenisSDevart <deniss.devart@gmail.com>
2025-04-25 12:13:14 +0300

    h

---
+++ Tables/Sales.Orders.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [Sales].[Orders] (
+  [OrderID] [int] NOT NULL,
+  [CustomerID] [int] NOT NULL,
+  [OrderDate] [date] NOT NULL,
+  [Amount] [money] NOT NULL,
+  PRIMARY KEY CLUSTERED ([OrderID])
+)
+ON [PRIMARY]
+GO
+
+ALTER TABLE [Sales].[Orders]
+  ADD CONSTRAINT [FK_Orders_Customers] FOREIGN KEY ([CustomerID]) REFERENCES [Sales].[Customers] ([CustomerID])
+GO
