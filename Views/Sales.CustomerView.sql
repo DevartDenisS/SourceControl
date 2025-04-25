a90fea671866f11984f4cd97b216b0c021abe6eb
DenisSDevart <deniss.devart@gmail.com>
2025-04-25 12:15:16 +0300

    v

--- Views/Sales.CustomerView.sql
+++ Views/Sales.CustomerView.sql
@@ -4,3 +4,6 @@ GO
 CREATE VIEW [Sales].[CustomerView] AS
 SELECT CustomerID, Name, Email FROM Sales.Customers;
 GO
+
+EXEC sys.sp_addextendedproperty N'MS_Description', N'Simple view for listing customers', 'SCHEMA', N'Sales', 'VIEW', N'CustomerView'
+GO
