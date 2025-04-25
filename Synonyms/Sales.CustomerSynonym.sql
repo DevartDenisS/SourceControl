36c0541afc69cfe6b7d42d4a643381bd89fd00a8
DenisSDevart <deniss.devart@gmail.com>
2025-04-25 12:18:06 +0300

    r

---
+++ Synonyms/Sales.CustomerSynonym.sql
@@ -0,0 +1,5 @@
+﻿CREATE SYNONYM [Sales].[CustomerSynonym] FOR [Sales].[Customers]
+GO
+
+EXEC sys.sp_addextendedproperty N'MS_Description', N'Synonym for Sales.Customers table', 'SCHEMA', N'Sales', 'SYNONYM', N'CustomerSynonym'
+GO
