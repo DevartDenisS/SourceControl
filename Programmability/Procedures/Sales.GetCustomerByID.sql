f3fedfbfe530b12829d3a3fb6d396649bc08aa46
DenisSDevart <deniss.devart@gmail.com>
2025-04-25 12:15:36 +0300

    вап

---
+++ Programmability/Procedures/Sales.GetCustomerByID.sql
@@ -0,0 +1,10 @@
+﻿SET QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+-- Create procedure
+CREATE PROCEDURE [Sales].[GetCustomerByID]
+    @CustomerID INT
+AS
+BEGIN
+    SELECT * FROM Sales.Customers WHERE CustomerID = @CustomerID;
+END;
+GO
