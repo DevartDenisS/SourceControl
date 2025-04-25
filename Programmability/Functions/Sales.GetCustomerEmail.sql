6558de1c04b8879011af01d6c35d336172e592cf
DenisSDevart <deniss.devart@gmail.com>
2025-04-25 12:16:15 +0300

    cv

---
+++ Programmability/Functions/Sales.GetCustomerEmail.sql
@@ -0,0 +1,15 @@
+﻿SET QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+-- Create scalar function
+CREATE FUNCTION [Sales].[GetCustomerEmail] (@CustomerID INT)
+RETURNS NVARCHAR(100)
+AS
+BEGIN
+    DECLARE @Email NVARCHAR(100);
+    SELECT @Email = Email FROM Sales.Customers WHERE CustomerID = @CustomerID;
+    RETURN @Email;
+END;
+GO
+
+EXEC sys.sp_addextendedproperty N'MS_Description', N'Returns the email of a customer by ID', 'SCHEMA', N'Sales', 'FUNCTION', N'GetCustomerEmail'
+GO
