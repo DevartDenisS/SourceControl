ab5ee108de10b1f558d24e140ea1de81f52c12a7
DenisSDevart <deniss.devart@gmail.com>
2025-04-25 12:11:57 +0300

    1

---
+++ Tables/Sales.Customers.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [Sales].[Customers] (
+  [CustomerID] [int] NOT NULL,
+  [Name] [nvarchar](100) NOT NULL,
+  [Email] [nvarchar](100) NOT NULL,
+  [Phone] [nvarchar](20) NULL,
+  PRIMARY KEY CLUSTERED ([CustomerID])
+)
+ON [PRIMARY]
+GO
