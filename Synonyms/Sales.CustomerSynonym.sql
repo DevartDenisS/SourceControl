b86a91c3981a0ff9ad7ba6358967c0e77ff1fd30
DevartDenisS <132042766+DevartDenisS@users.noreply.github.com>
2026-02-27 10:48:21 +0200

    Delete Synonyms directory

--- Synonyms/Sales.CustomerSynonym.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE SYNONYM [Sales].[CustomerSynonym] FOR [Sales].[Customers]
-GO
-
-EXEC sys.sp_addextendedproperty N'MS_Description', N'Synonym for Sales.Customers table', 'SCHEMA', N'Sales', 'SYNONYM', N'CustomerSynonym'
-GO
